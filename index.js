0595d80504c7b68c7ebe3fee4de0ae4a075ed532
ayaz40 <ayazpubgwp@gmail.com>
2023-09-10 13:44:53 +0500

    my changes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const path = require("path")
 require("dotenv").config()
-const path = process.env.PORT || 8080
+const port = process.env.PORT || 8080
 const allroutes = require("./routes/all")
 const assets = path.join(__dirname,"assets")
-app.set("view engin","ejs")
+app.set("view engine","ejs")
 app.use("/style",express.static(path.join(assets,"style")))
 app.use("/img",express.static(path.join(assets,"img")))
 app.use("/",allroutes)
--- routes/all.js
+++ routes/all.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const routes = express.Router()
 const home = require("./home")
-const about = require("/about")
+const about = require("./about")
 const contact = require("./contact")
 routes.use("/",home)
 routes.use("/about",about)
