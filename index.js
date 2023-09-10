91f0f47a332fcbd6f1ed363edb7ce8d4e0511242
ayaz40 <ayazpubgwp@gmail.com>
2023-09-10 13:35:13 +0500

    commit before running app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+require("dotenv").config()
+const path = process.env.PORT || 8080
+const allroutes = require("./routes/all")
+const assets = path.join(__dirname,"assets")
+app.set("view engin","ejs")
+app.use("/style",express.static(path.join(assets,"style")))
+app.use("/img",express.static(path.join(assets,"img")))
+app.use("/",allroutes)
+app.listen(port,()=>{
+    console.log(`this is server connection http://localhost:${port}`)
+})
